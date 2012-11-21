d65fcce29d60c21b8f35ddfe1a6aedb82912d3eb
Sheetal Nandi <shkamat@microsoft.com>
2012-11-21 13:30:41 -0800

    Update LKG

--- bin/typescriptServices.js
+++ bin/typescriptServices.js
@@ -21701,9 +21701,23 @@ var TypeScript;
     TypeScript.getPrettyName = getPrettyName;
     function getRelativePathToFixedPath(fixedModFilePath, absoluteModPath) {
         absoluteModPath = switchToForwardSlashes(absoluteModPath);
-        var fileNameIndex = absoluteModPath.indexOf(fixedModFilePath);
-        if(fileNameIndex == 0) {
-            return absoluteModPath.substring(fixedModFilePath.length);
+        var modComponents = absoluteModPath.split("/");
+        var fixedModComponents = fixedModFilePath.split("/");
+        var joinStartIndex = 0;
+        for(; joinStartIndex < modComponents.length && joinStartIndex < fixedModComponents.length; joinStartIndex++) {
+            if(fixedModComponents[joinStartIndex] != modComponents[joinStartIndex]) {
+                break;
+            }
+        }
+        if(joinStartIndex != 0) {
+            var relativePath = "";
+            var relativePathComponents = modComponents.slice(joinStartIndex, modComponents.length);
+            for(; joinStartIndex < fixedModComponents.length; joinStartIndex++) {
+                if(fixedModComponents[joinStartIndex] != "") {
+                    relativePath = relativePath + "../";
+                }
+            }
+            return relativePath + relativePathComponents.join("/");
         }
         return absoluteModPath;
     }
