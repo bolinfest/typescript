8c8e9180b59b53a9d1795e47fd095de0dbf28fc7
Michael Bolin <bolinfest@gmail.com>
2013-01-01 17:44:59 -0800

    Display errors from the TypeScript compiler in the demo UI.

--- closure/demo/demo.js
+++ closure/demo/demo.js
@@ -66,7 +66,7 @@ demo.compile = function() {
   var checkbox = get('enable-google');
 
   var value = '';
-  var error;
+  var error = null;
   try {
     var errorOutput = demo.createITextWriter();
     var logger = new TypeScript.NullLogger();
@@ -74,9 +74,7 @@ demo.compile = function() {
     settings.outputGoogleClosureAnnotations = checkbox.checked;
 
     var compiler = new TypeScript.TypeScriptCompiler(errorOutput, logger, settings);
-    // TODO(bolinfest): Configure error-handling for TypeScriptCompiler.
-
-    var filenameForErrorReportingPurposes = 'textarea.ts';
+    var filenameForErrorReportingPurposes = '<INPUT>';
     compiler.addUnit(input, filenameForErrorReportingPurposes);
     compiler.typeCheck();
 
@@ -91,13 +89,19 @@ demo.compile = function() {
 
     value = originalTextWriter.getOutput();
     demo.jsEditor.getSession().setValue(value);
+
+    if (errorOutput.getOutput()) {
+      error = errorOutput.getOutput();
+    }
   } catch (e) {
-    error = e;
-    get('error').innerHTML = goog.string.htmlEscape(e.message);
+    error = e.message;
   }
 
   // Update the UI to reflect whether there is an error. 
   var isError = (error != null);
+  if (isError) {
+    get('error').innerHTML = goog.string.htmlEscape(error);
+  }
   get('error').style.visibility = isError ? 'visible' : 'hidden';
   // TOOD(bolinfest): Figure out why the text does not turn red.
   goog.dom.classes.enable(get('code-coffee'), 'error', isError);
